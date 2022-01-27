de1cb3b1ce457304ab5f6636bd6048dcff2db10c
feroz-delixus <77040775+feroz-delixus@users.noreply.github.com>
2022-01-27 11:48:45 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const server = express()
 
 server.use((req, res) => {
-  res.send('Hello World!')
+  res.send('Hello Welcome to delixus!')
 })
 
 const PORT = process.env.PORT || 3000
